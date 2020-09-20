3fd12b34fd7fdff4234d0e69346c37f2548b3955
Gourang Nagar <65488240+krypto6402@users.noreply.github.com>
2020-09-20 14:47:23 +0530

    Update 1.Image-Video-Webcam.py

--- Face Recogination/1.Image-Video-Webcam.py
+++ Face Recogination/1.Image-Video-Webcam.py
@@ -17,4 +17,16 @@ while True:
         break
 
 
-s        
+# Webcam
+import cv2
+
+vid = cv2.VideoCapture(0)
+vid.set(3,640)   # Width of screen
+vid.set(4,480)   # Hight of screen
+vid.set(10,100)  # Brightness of Webcam
+
+while True:
+    success, img = vid.read()
+    cv2.imshow("Video",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):   # press q to stop webcam
+        break       

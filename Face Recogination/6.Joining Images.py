f6f54af4658e906cffca508ecf77e366a108871e
Gourang Nagar <65488240+krypto6402@users.noreply.github.com>
2020-09-21 03:37:37 +0530

    Update 6.Joining Images.py

--- Face Recogination/6.Joining Images.py
+++ Face Recogination/6.Joining Images.py
@@ -1,1 +1,12 @@
-s
+import cv2
+import numpy as np
+
+img = cv2.imread("Resources/test.jpeg")
+
+imghor = np.hstack((img,img)) # Join Images Horizontally
+imgver = np.vstack((img,img)) # Join Image Vertically
+
+cv2.imshow("Horizontal",imghor)
+cv2.imshow("Vertical",imgver)
+
+cv2.waitKey(0)
